07fd2ff89e9bd03cb013fa3855b9d06114246f3e
Paul <paul_lodder@live.nl>
2021-01-08 12:35:18 +0100

    Update script to use bash and include instructions in readme

--- scripts/get_all.sh
+++ scripts/get_all.sh
@@ -1,7 +1,7 @@
 CURRENT_DIR=$pwd
 THIS_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-sh $THIS_DIR/get_adult.sh
-sh $THIS_DIR/get_german.sh
-sh $THIS_DIR/get_cifar10.sh
-sh $THIS_DIR/get_cifar100.sh
-sh $THIS_DIR/get_yaleb.sh
+bash $THIS_DIR/get_adult.sh
+bash $THIS_DIR/get_german.sh
+bash $THIS_DIR/get_cifar10.sh
+bash $THIS_DIR/get_cifar100.sh
+bash $THIS_DIR/get_yaleb.sh
